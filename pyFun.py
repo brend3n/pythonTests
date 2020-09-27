ad1cb6b7581003399f5e17ea4c0d4830e57082da
brend3n <brenden@knights.ucf.edu>
2020-09-27 18:14:15 -0400

    Added some exercises with basic stuff

---
+++ pyFun.py
@@ -0,0 +1,32 @@
+# print("Enter a name: ")
+# name = input()
+# print("Hi,",name)
+#
+# print("How hot are you: ")
+# hot_lvl = input()
+# hot_lvl = int(hot_lvl)
+# if hot_lvl < 1:
+# 	print("I am so sorry")
+# elif hot_lvl >= 1 and hot_lvl <= 3:
+# 	print("You kind of ugly I guess")
+# else:
+# 	print("You HOT af papi")
+#
+#
+# str = ''
+# while str != 'penis':
+# 	print("Enter penis, pls")
+# 	str = input()
+# print("Thanks, bitch")
+
+
+# import random
+# num = -1
+# counts = 0
+# avg = 0
+#
+# while num != 69:
+# 	# print(num)
+# 	num = random.randint(1,1000)
+# 	counts += 1
+# print("That took: " + str(counts) + " iterations")
